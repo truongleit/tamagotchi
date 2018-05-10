642e3bf939c8488fb5703a3b2eff7a5ad2590943
Unknown <it.lesontruong@gmail.com>
2018-05-11 00:43:21 +0200

    fix interval and build updateBar function

--- src/main.js
+++ src/main.js
@@ -7,14 +7,94 @@ import './assets/css/style.css'
 
 Vue.config.productionTip = false
 
-new Vue({
+var properties = new Vue({
+    el: '#example-2',
     data: {
-        health: 100
+        health: 100,
+        hungry: 100,
+        thirsty: 100,
+        happiness: 100,
+        entertainment: 100,
+        losingHealth: function() {
+            var self = this;
+            setInterval(function() {
+                if (self.health > 0) {
+                    var type = 'health';
+                    self.health = self.health - 0.05;
+                    updateBar(type, self.health);
+                    console.log(self.health);
+                } else {
+                    clearInterval;
+                }
+            }, 50);
+            setInterval(function() {
+                if (self.hungry > 0) {
+                    var type = 'hunger';
+                    self.hungry = self.hungry - 0.05;
+                    updateBar(type, self.hungry);
+                    console.log(self.hungry);
+                } else {
+                    clearInterval;
+                }
+            }, 50);
+            setInterval(function() {
+                if (self.thirsty > 0) {
+                    var type = 'thirst';
+                    self.thirsty = self.thirsty - 0.05;
+                    updateBar(type, self.thirsty);
+                    console.log(self.thirsty);
+                } else {
+                    clearInterval;
+                }
+            }, 50);
+            setInterval(function() {
+                if (self.happiness > 0) {
+                    var type = 'happiness';
+                    self.happiness = self.happiness - 0.05;
+                    updateBar(type, self.happiness);
+                    console.log(self.happiness);
+                } else {
+                    clearInterval;
+                }
+            }, 50);
+        }
     },
-    created: function() {
-        while (this.health > 0) {
-            console.log(this.health);
-            this.health--;
+    methods: {
+        eat: function() {
+            if (this.health < 100) {
+                this.health += 15;
+                if (this.health > 100) {
+                    this.health = 100;
+                }
+                updateBar('health', this.health);
+                console.log(this.health);
+            } else if (this.health > 100) {
+                this.health = 100;
+                updateBar('health', this.health);
+            }
+
         }
     }
 });
+
+function updateBar(type, percent) {
+    switch (type) {
+        case 'health':
+            $('.health .progress-bar').css('width', percent + '%');
+            break;
+        case 'hunger':
+            $('.hungry .progress-bar').css('width', percent + '%');
+            break;
+        case 'thirst':
+            $('.thirsty .progress-bar').css('width', percent + '%');
+            break;
+        case 'happiness':
+            $('.happy .progress-bar').css('width', percent + '%');
+            break;
+        case 'experience':
+            $('.experience .progress-bar').css('width', percent + '%');
+            break;
+    }
+}
+
+properties.losingHealth();
