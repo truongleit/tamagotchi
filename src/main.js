96a3225ad304513b28d9c2c6104216a431da6c5c
Unknown <it.lesontruong@gmail.com>
2018-05-15 02:53:16 +0200

    finish intervalID array

--- src/main.js
+++ src/main.js
@@ -67,6 +67,7 @@ var character = new Vue({
     methods: {
         stopMusic: function() {
             if (!this.isPlaying) {
+                $('.player').attr('autoplay', 'autoplay');
                 this.isPlaying = true;
                 $('.player')[0].play();
                 this.currentTrackName = getTrackName(this.currentTrack, this.music);
@@ -132,17 +133,21 @@ function getTrackURL(no, list) {
 
 var properties = new Vue({
     data: {
-        beMonster: false,
+        beMonster: true,
         health: 100,
         hungry: 100,
         thirsty: 100,
         happiness: 100,
         entertainment: 100,
+        intervalID: [],
         losingHealth: function() {
             var self = this;
             if (this.beMonster) {
-                setInterval(function() {
+                var health = setInterval(function() {
                     if (self.health > 0 && self.hungry < 80 && self.thirsty < 70) {
+                        if (self.intervalID.length == 0) {
+                            self.intervalID[0] = health;
+                        }
                         var type = 'health';
                         self.health = self.health - 0.25;
                         updateBar(type, self.health);
@@ -150,8 +155,11 @@ var properties = new Vue({
                         clearInterval;
                     }
                 }, 50);
-                setInterval(function() {
+                var hunger = setInterval(function() {
                     if (self.hungry > 0) {
+                        if (self.intervalID.length == 1) {
+                            self.intervalID[1] = hunger;
+                        }
                         var type = 'hunger';
                         self.hungry = self.hungry - 0.25;
                         updateBar(type, self.hungry);
@@ -159,8 +167,11 @@ var properties = new Vue({
                         clearInterval;
                     }
                 }, 50);
-                setInterval(function() {
+                var thirst = setInterval(function() {
                     if (self.thirsty > 0) {
+                        if (self.intervalID.length == 2) {
+                            self.intervalID[2] = thirst;
+                        }
                         var type = 'thirst';
                         self.thirsty = self.thirsty - 0.25;
                         updateBar(type, self.thirsty);
@@ -168,8 +179,11 @@ var properties = new Vue({
                         clearInterval;
                     }
                 }, 50);
-                setInterval(function() {
+                var happiness = setInterval(function() {
                     if (self.happiness > 0) {
+                        if (self.intervalID.length == 3) {
+                            self.intervalID[3] = happiness;
+                        }
                         var type = 'happiness';
                         self.happiness = self.happiness - 0.2;
                         updateBar(type, self.happiness);
@@ -320,6 +334,7 @@ var drinkLoading = new Vue({
     },
     methods: {
         drinking: function(point) {
+            // Calculating experience point and level up //
             var check = character.nextLevel - character.experience;
             if (point > check) {
                 character.experience = point - check;
@@ -372,6 +387,8 @@ var musicLoading = new Vue({
     el: '#music',
     data: {
         currentClass: '',
+        pausedByOther: false,
+        inProgress: false,
         items: [{
                 name: 'Cello',
                 point: 30,
@@ -432,69 +449,85 @@ var musicLoading = new Vue({
     },
     methods: {
         listening: function(point, className, url, audio) {
-            // Remove old position class //
-            if (this.currentClass != '') {
-                var oldClass = this.currentClass;
-                $('.music-instrument').addClass('zoomOut');
-                setTimeout(function() {
-                    $('.music-instrument').removeClass('zoomOut');
-                    $('.instrument-note').removeClass('zoomOut');
-                    $('.music-instrument').removeClass(oldClass);
-                    $('.music-instrument').removeClass('block animated zoomIn');
-                    $('.instrument-note').removeClass(oldClass);
-                    $('.instrument-note').removeClass('block');
-                }, 500);
-                this.currentClass = className;
-            } else {
-                this.currentClass = className;
-            }
-            // Add new position class and new svg //
-            setTimeout(function() {
-                $('.music-instrument img').attr('src', url);
-                $('.music-instrument').addClass(className + ' block animated zoomIn');
-                $('.instrument-note').addClass('block ' + className);
-                $('.instrument-audio').attr({
-                    src: audio,
-                    autoplay: 'autoplay'
-                });
-            }, 500);
-            // Calculating experience point and level up //
-            var check = character.nextLevel - character.experience;
-            if (point > check) {
-                character.experience = point - check;
-                character.nextLevel += 200;
-                character.level++;
-                var percent = (character.experience / character.nextLevel) * 100;
-                updateBar('experience', 100);
-                $('.firework').addClass('block');
-                $('.experience .progress-bar').addClass('no-transition');
-                updateBar('experience', 0);
-                setTimeout(function() {
-                    $('.experience .progress-bar').removeClass('no-transition');
-                    updateBar('experience', percent);
-                }, 250);
-                setTimeout(function() {
-                    $('.firework').removeClass('block');
-                }, 4000);
-            } else {
-                character.experience += point;
-                var percent = (character.experience / character.nextLevel) * 100;
-                updateBar('experience', percent);
-            }
-            // Changing status of HAPPINESS bar //
-            if (properties.happiness < 100) {
-                properties.happiness += 15;
-                if (properties.happiness > 100) {
-                    properties.happiness = 100;
+            if (!this.inProgress) {
+                this.inProgress = true;
+                // Stop the background music if it's playing //
+                var otherIsPaused = $('.player').prop('paused');
+                var hasAutoplay = $('.player')[0].hasAttribute('autoplay');
+                if (!otherIsPaused && hasAutoplay) {
+                    character.stopMusic();
+                    this.pausedByOther = true;
                 }
-                updateBar('happiness', properties.happiness);
-            } else if (properties.happiness > 100) {
-                properties.happiness = 100;
-                updateBar('happiness', properties.happiness);
+                // Remove old position class //
+                if (this.currentClass != '') {
+                    var oldClass = this.currentClass;
+                    $('.music-instrument').addClass('zoomOut');
+                    setTimeout(function() {
+                        $('.music-instrument').removeClass('zoomOut');
+                        $('.instrument-note').removeClass('zoomOut');
+                        $('.music-instrument').removeClass(oldClass);
+                        $('.music-instrument').removeClass('block animated zoomIn');
+                        $('.instrument-note').removeClass(oldClass);
+                        $('.instrument-note').removeClass('block');
+                    }, 500);
+                    this.currentClass = className;
+                } else {
+                    this.currentClass = className;
+                }
+                // Add new position class and new svg //
+                setTimeout(function() {
+                    $('.music-instrument img').attr('src', url);
+                    $('.music-instrument').addClass(className + ' block animated zoomIn');
+                    $('.instrument-note').addClass('block ' + className);
+                    $('.instrument-audio').attr({
+                        src: audio,
+                        autoplay: 'autoplay'
+                    });
+                }, 500);
+                // Calculating experience point and level up //
+                var check = character.nextLevel - character.experience;
+                if (point > check) {
+                    character.experience = point - check;
+                    character.nextLevel += 200;
+                    character.level++;
+                    var percent = (character.experience / character.nextLevel) * 100;
+                    updateBar('experience', 100);
+                    $('.firework').addClass('block');
+                    $('.experience .progress-bar').addClass('no-transition');
+                    updateBar('experience', 0);
+                    setTimeout(function() {
+                        $('.experience .progress-bar').removeClass('no-transition');
+                        updateBar('experience', percent);
+                    }, 250);
+                    setTimeout(function() {
+                        $('.firework').removeClass('block');
+                    }, 4000);
+                } else {
+                    character.experience += point;
+                    var percent = (character.experience / character.nextLevel) * 100;
+                    updateBar('experience', percent);
+                }
+                // Changing status of HAPPINESS bar //
+                if (properties.happiness < 100) {
+                    properties.happiness += 15;
+                    if (properties.happiness > 100) {
+                        properties.happiness = 100;
+                    }
+                    updateBar('happiness', properties.happiness);
+                } else if (properties.happiness > 100) {
+                    properties.happiness = 100;
+                    updateBar('happiness', properties.happiness);
+                }
             }
         },
         instrumentEnd: function() {
+            var self = this;
             $('.instrument-audio').bind("ended", function() {
+                self.inProgress = false;
+                if (self.pausedByOther) {
+                    character.stopMusic();
+                    self.pausedByOther = false;
+                }
                 this.currentClass = '';
                 $('.music-instrument').addClass('zoomOut');
                 $('.instrument-note').removeClass('block');
